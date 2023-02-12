f72863858c0b4afa7318a2deda917fbbcaf4dff5
Josip Džinić <jdzinic@racunarstvo.hr>
2023-02-13 00:59:34 +0100

    insert

---
+++ src/authConfig.js
@@ -0,0 +1,21 @@
+export const msalConfig = {
+    auth: {
+      clientId: "eb97a5d9-ba57-40fb-a0f6-62eef27beed7",
+      authority: "https://login.microsoftonline.com/commmon", 
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+  
+  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+  export const loginRequest = {
+   scopes: ["User.Read"]
+  };
+  
+  // Add the endpoints here for Microsoft Graph API services you'd like to use.
+  export const graphConfig = {
+      graphMeEndpoint: "https://graph.microsoft.com"
+  };
