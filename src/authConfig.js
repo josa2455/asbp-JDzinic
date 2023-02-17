5a85a70df7ddcb415ae360c63a3a90154c7d9169
Josip Džinić <jdzinic@racunarstvo.hr>
2023-02-18 00:16:56 +0100

    final

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,21 +1,19 @@
 export const msalConfig = {
     auth: {
       clientId: "eb97a5d9-ba57-40fb-a0f6-62eef27beed7",
-      authority: "https://login.microsoftonline.com/3f8c3345-0ff1-4cca-88a9-2d4002ad312d", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      authority: "https://login.microsoftonline.com/3f8c3345-0ff1-4cca-88a9-2d4002ad312d",
       redirectUri: "http://localhost:3000",
     },
     cache: {
-      cacheLocation: "sessionStorage", // This configures where your cache will be stored
-      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+      cacheLocation: "sessionStorage",
+      storeAuthStateInCookie: false,
     }
   };
   
-  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
   export const loginRequest = {
    scopes: ["User.Read"]
   };
   
-  // Add the endpoints here for Microsoft Graph API services you'd like to use.
   export const graphConfig = {
       graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
   };
--- src/graph.js
+++ src/graph.js
@@ -1,8 +1,6 @@
 import { graphConfig } from "./authConfig";
 
-/**
- * Attaches a given access token to a Microsoft Graph API call. Returns information about the user
- */
+
 export async function callMsGraph(accessToken) {
     const headers = new Headers();
     const bearer = `Bearer ${accessToken}`;
